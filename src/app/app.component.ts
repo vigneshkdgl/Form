7e2d5d3a3cd0e272e25585bdb778e560e1b09b1d
vigneshkdgl <vigneshwaran5481@gmail.com>
2020-06-27 14:37:06 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ApplicationForm';
+  book: any = { };
+
+   constructor(http: HttpClient) {
+     http.get('https://api.angular.schule/book/9783864906466')
+       .subscribe(b => this.book = b);
+   }
+}
